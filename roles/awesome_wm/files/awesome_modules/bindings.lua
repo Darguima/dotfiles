dcbe9b7549cbe2dbd6f865d67cda362c8f607d30
Darguima <dsgdevbraga@gmail.com>
2024-02-15 12:26:35 +0000

    [awesome_wm] - nautilus shortcut

--- roles/awesome_wm/files/awesome_modules/bindings.lua
+++ roles/awesome_wm/files/awesome_modules/bindings.lua
@@ -7,6 +7,8 @@ local function global_keys(modkey)
   -- {{{ My bindings
     awful.key({ modkey, }, "f", function() awful.spawn("firefox") end,
       { description = "open firefox", group = "launcher" }),
+    awful.key({ modkey, }, "z", function() awful.spawn("nautilus") end,
+      { description = "open nautilus", group = "launcher" }),
     awful.key({ modkey, }, "d", function() awful.spawn("rofi -show run"); end,
       { description = "rofi", group = "launcher" }),
     awful.key({ modkey, }, "e", function() awful.spawn("emote"); end,
