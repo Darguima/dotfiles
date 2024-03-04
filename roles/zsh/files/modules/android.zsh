d81b0a2b4f19f3dc97a2d17a896fa6290d81ea2b
Darguima <dsgdevbraga@gmail.com>
2024-03-04 12:05:58 +0000

    [android_sdk] - created role [android_emulator] - created role

---
+++ roles/zsh/files/modules/android.zsh
@@ -0,0 +1,7 @@
+# Android SDK & Emulator
+
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk/
+export ANDROID_SDK_ROOT=/opt/android-sdk
+
+alias phone="emulator -avd phone"
+# alias tablet="emulator -avd tablet"
