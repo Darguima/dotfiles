aea8d969250ec5dcefdeca34459bfc99f4d4a861
Darguima <dsgdevbraga@gmail.com>
2025-01-19 21:04:48 +0000

    [zsh] - Remove DroidCam aliases (init mic/cam)

--- roles/zsh/files/modules/aliases.zsh
+++ roles/zsh/files/modules/aliases.zsh
@@ -9,8 +9,9 @@ alias co="code ."
 
 alias upgrade="yay -Suy --devel --noconfirm"
 
-alias init_mic0="pacmd load-module module-alsa-source device=hw:Loopback,1,0"
-alias init_mic1="pacmd load-module module-alsa-source device=hw:Loopback,1,1"
-alias kill_mics="pacmd unload-module module-alsa-source"
-alias init_cam="echo \"vm\" | env ANDROID_SERIAL=A3201809023158 droidcam-cli -a -v -dev=/dev/video0 adb 4747"
+# DroidCam
+# alias init_mic0="pactl load-module module-alsa-source device=hw:0,1,0"
+# alias init_mic1="pactl load-module module-alsa-source device=hw:0,1,1"
+# alias kill_mics="pactl unload-module module-alsa-source"
+# alias init_cam="echo \"vm\" | env ANDROID_SERIAL=A3201809023158 droidcam-cli -a -v -dev=/dev/video0 adb 4747"
 # alias mic_test="LBID=$(pactl load-module module-loopback); sleep 1; pactl unload-module $LBID"
